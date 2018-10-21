4f407cbc313adf64bd0fd360b1401f3f14e4053c
Francisco <francisnardi@gmail.com>
2018-10-21 06:08:30 -0300

    novo6

--- hack5a.py
+++ hack5a.py
@@ -112,6 +112,26 @@ def availableServicesForInternet():
          ]
      })
 
+
+@app.route('/servicesListForPhone')
+def servicesListForPhone():
+    return jsonify(
+     {
+         "id" : 1,
+         "name" : "Fixo Local",
+         "price" : 99.00
+     },
+     {
+         "id" : 2,
+         "name" : "Fixo + Móvel Local",
+         "price" : 129.00
+     },
+     {
+         "id" : 3,
+         "name" : "Fixo + Móvel Brasil",
+         "price" : 149.00
+     }
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
