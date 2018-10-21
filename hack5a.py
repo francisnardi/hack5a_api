5c0f552ece07446662d5ad3725c154e1664adf71
Francisco <francisnardi@gmail.com>
2018-10-21 04:22:44 -0300

    update function name

--- hack5a.py
+++ hack5a.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
-def nao_entre_em_panico():
+def hack5a():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
     return jsonify({"message": "Não entre em pânico!"})
