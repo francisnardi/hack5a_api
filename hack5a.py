6cc71401dea8bec9871d13c7410ab093c2997561
Francisco <francisnardi@gmail.com>
2018-10-21 05:15:06 -0300

    novo3

--- hack5a.py
+++ hack5a.py
@@ -4,24 +4,29 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/1')
-def hack5a():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "a"})
-
-@app.route('/2')
-def hack5a2():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "b"})
-
-@app.route('/3')
-def hack5a3():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "c"})
-
+@app.route('/serviceTypeList')
+def serviceTypeList():
+    return jsonify(
+    {
+        "id":1,
+        "name": "Internet, telefone, TV a cabo",
+        "imageName" : "Internet"
+    },
+    {
+    	"id": 2,
+    	"name": "Manutenção",
+        "imageName" : "Maintenance"
+    },
+    {
+    	"id": 3,
+    	"name": "Chaves",
+        "imageName" : "Keys"
+    },
+    {
+    	"id": 4,
+    	"name": "Seguro",
+        "imageName" : "Assurance"
+    })
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
