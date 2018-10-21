5ded017d8afffe89c76436a92301177735b80a8a
Francisco <francisnardi@gmail.com>
2018-10-21 05:02:48 -0300

    novo

--- hack5a.py
+++ hack5a.py
@@ -6,9 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hack5a():
-    if request.headers.get('Authorization') == '42':
-        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    if request.headers.get('Authorization') == '40':
+        return jsonify({"40": "A vingança nunca é plena, mata a alma e envenena."})
+    return jsonify({"message": "4040404040"})
+
+@app.route('/1')
+def hack5a():
+    if request.headers.get('Authorization') == '30':
+        return jsonify({"30": "A vingança nunca é plena, mata a alma e envenena."})
+    return jsonify({"message": "3030303030"})
+
+@app.route('/2')
+def hack5a():
+    if request.headers.get('Authorization') == '20':
+        return jsonify({"20": "A vingança nunca é plena, mata a alma e envenena."})
+    return jsonify({"message": "2020202020"})
+
+@app.route('/3')
+def hack5a():
+    if request.headers.get('Authorization') == '10':
+        return jsonify({"10": "A vingança nunca é plena, mata a alma e envenena."})
+    return jsonify({"message": "1010101010"})
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
